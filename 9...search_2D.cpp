9cf978f14f4f0a36ab0c22867ce65b06f0538a74
Ayush Kumar Rathor <90344884+Rathore887@users.noreply.github.com>
2023-06-23 23:52:47 +0530

    Update 9...search_2D.cpp

--- 9...search_2D.cpp
+++ 9...search_2D.cpp
@@ -1,1 +1,34 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int index = 0;
+        for (int i = 0; i < row; i++)
+        {
+            if (matrix[i][0] <= target && target <= matrix[i][col - 1])
+            {
+                index = i;
+            }
+        }
+        for (int j = 0; j < col; j++)
+        {
+            if (matrix[index][j] == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    return 0;
+}
