0a62ef56743de7a80f64db23e59adee5817729d2
Ayush Kumar Rathor <90344884+Rathore887@users.noreply.github.com>
2023-06-23 23:53:24 +0530

    Update 7..sorting.cpp

--- 7..sorting.cpp
+++ 7..sorting.cpp
@@ -1,1 +1,26 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+void sorting(vector<int> &nuns)
+ {
+     int р0 =0, pl =0, p2 - nums.size() - 1;
+    while (p1<=p2)
+     {
+         switch (nums[p1])
+         case 0:
+             swap (nums [pe], nums (p1));
+            p0++;
+            p1++;
+            break
+        case 1:
+            p1++;
+             break;
+        case 2:
+            swap(nums [p1], nums[p2]);
+            р2--;
+            break;
+     }
+ }
+ int main(){
+
+ }
