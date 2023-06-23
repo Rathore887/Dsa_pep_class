b214918f778fa1325981d29e7fd48be0ea858fee
Ayush Kumar Rathor <90344884+Rathore887@users.noreply.github.com>
2023-06-23 23:53:14 +0530

    Update 8...set_matrix_zero.cpp

--- 8...set_matrix_zero.cpp
+++ 8...set_matrix_zero.cpp
@@ -1,1 +1,42 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+
+        vector<int> arr1(matrix.size(), -1);
+        vector<int> arr2(matrix[0].size(), -1);
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    arr1[i] = 0;
+                    arr2[j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (arr1[i] == 0 || arr2[j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
