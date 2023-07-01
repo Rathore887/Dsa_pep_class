1922dec1beca97af6cee04c7bfc5a722b44651d2
Ayush Kumar Rathor <rathoreayush2905@gmail.com>
2023-07-02 01:11:05 +0530

    adding

---
+++ 20_mergeInterval.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        int n=intervals.size();
+        vector<int>v=intervals[0];
+       for(auto x:intervals)
+       {
+           if(x[0]<=v[1])
+           {
+               v[1]=max(v[1],x[1]);
+           }
+           else
+           {
+               ans.push_back(v);
+               v=x;
+           }
+       }
+       ans.push_back(v);
+       return ans;
+    }
+};
+
+int main()
+{ 
+    return 0;
+}
